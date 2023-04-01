e1400fc4d2d37c6dc6729c372eba05906282d18b
Jerome Flores <jpeterflores.24.@gmail.com>
2023-04-01 19:50:48 +0800

    fix some errors and adding contact page

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+
+                        // CATEGORY PRODUCT SCROLLING
+
+
 let scrollPosition = 0;
 let scrollAmount = 200;
 
@@ -9,9 +13,9 @@ let scrollAmount = 200;
     function scrollHorizontal(amt){
             scrollPosition += (amt * scrollAmount);
 
-            // if (scrollPosition > 0){
-            //     scrollPosition = 0;
-            // }
+            if (scrollPosition > 0){
+                scrollPosition = 0;
+            } 
             
             // if (scrollPostion < maxScroll) {
             //     scrollPosition = maxScroll;
@@ -23,5 +27,97 @@ let scrollAmount = 200;
 
 
 
+                        // FOR HISTORY API
 
-        
+    window.history.replaceState({ page: 'homepage'}, 'homepage', '#homepage');
+
+    function nextContent(pageName) {
+        const pages = document.getElementsByClassName('page');
+        for (let p of pages) {
+            p.classList.add('hidden');
+        }
+
+        const currentPage = document.getElementById(pageName);
+        currentPage.classList.remove('hide');
+    }
+
+    function navigate(pageName) {
+        windows.history.pushState({page: pageName}, pageName, `#${pageName}`);
+
+        nextContent(pageName);
+    }
+
+
+
+    window.history.replaceState({ page: 'homepage' }, 'homepage', '#homepage');
+
+   
+    function updateContent(pageName) {
+      
+        const pages = document.getElementsByClassName('page');
+        for (let p of pages) {
+            p.classList.add('hide');
+        }
+
+        const currentPage = document.getElementById(pageName);
+        currentPage.classList.remove('hide');
+
+    }
+
+
+    function navigate(pageName) {
+
+        window.history.pushState({ page: pageName }, pageName, `#${pageName}`);
+
+        updateContent(pageName);
+    }
+
+    window.addEventListener('popstate', function(e) {
+        if (e.state) {
+            updateContent(e.state.page);
+        }
+    });
+
+
+
+
+                         // FOR STORAGE API
+
+
+
+    //    const loggedinEmailAddress = localStorage.getItem('email');
+
+    //     updateContent(loggedinEmailAddress);
+
+    //     function updateContent(loggedInEmail) {
+    //         if (loggedInEmail) {
+    //             const loginForm = document.getElementById('login-form');
+    //             loginForm.classList.add('hide');
+
+    //             const loggedinSection = document.getElementById('loggedin-section');
+    //             loggedinSection.classList.remove('hidden');
+
+    //             const loggedinUser = document.getElementById('logout-btn');
+    //             loggedinUser.textContent = `Logout: ${loggedInEmail}`;
+    //         } else {
+    //             const loginForm = document.getElementById('login-form');
+    //             loginForm.classList.remove('hide');
+
+    //             const loggedinSection = document.getElementById('login-area');
+    //             loggedinSection.classList.add('hidden');
+    //         }
+    //     }
+
+    //     function login() {
+    //         const emailAddressInput = document.getElementById('email');
+
+    //         const emailAddress = emailAddressInput.value;
+
+    //         localStorage.setItem('email', emailAddress);
+    //         updateContent(emailAddress);
+    //     }
+
+    //     function logout() {
+    //         localStorage.removeItem('email');
+    //         updateContent(null);
+    //     }  
