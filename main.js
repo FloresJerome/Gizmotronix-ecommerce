c362fc5a629757b8ae11e3bfe744d0b14dcb3807
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-04-04 20:49:24 +0800

    :sparkles: Added fetching API feature for getting the product logs information

--- main.js
+++ main.js
@@ -1,3 +1,113 @@
+//Page 1
+
+fetchData('best-seller');
+fetchData('new-arrival')
+
+
+function fetchData(event) {
+    return fetch('https://raw.githubusercontent.com/FloresJerome/Gizmotronix-ecommerce/main/product-data.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response error');
+            }
+            return response.json();
+        })
+        .then(data => {
+
+            if (event === 'best-seller') {
+                for (let y = 1; y < 8; y++) {
+                    let newArrivalList = data.product;
+                    let newItem = newArrivalList[0].rating.sold;
+                    let count = 0;
+                    let itemIndex = 0;
+
+                    for (let item of newArrivalList) {
+                        if (newItem > item.rating.sold) {
+                            newItem = item.rating.sold;
+                            itemIndex = count;
+                        }
+                        count++;
+                    }
+                    const newArrival = document.getElementById('best-seller');
+                    const productContainer = document.createElement('div');
+                    productContainer.className = 'col-md-6 col-lg-3 mb-4';
+                    productContainer.innerHTML = `<div class="card">
+                                                    <div class="inner"><a href="#"><img class="card-img-top" src="${newArrivalList[itemIndex].image[0]}"></a></div>
+                                                    <div class="card-body p-2">
+                                                            <div>
+                                                                    <p class="card-text fw-bolder fs-4 text-center">${newArrivalList[itemIndex].title}</p><span class="fs-4">${newArrivalList[itemIndex].price.currency} ${newArrivalList[itemIndex].price.value}</span><br>
+                                                                    <span class="text-dark fs-6">${newArrivalList[itemIndex].stock != 0 ? 'Available' : 'Not Available'}</span>
+                            
+                                                                    <div class="d-flex justify-content-start text-warning my-4">
+                                                                            <div><i class="fa-solid fa-star"></i></div>
+                                                                            <div><i class="fa-solid fa-star"></i></div>
+                                                                            <div><i class="fa-solid fa-star"></i></div>
+                                                                            <div><i class="fa-solid fa-star"></i></div>
+                                                                            <div><i class="fa-solid fa-star-half-stroke"></i></div>
+                                                                            <span class="text-dark fs-6 ms-2">| ${newArrivalList[itemIndex].rating.sold} Sold</span>
+                                                                    </div>
+                                                            </div>
+                                                    </div>
+                                                </div>`;
+                    newArrival.appendChild(productContainer);
+
+                    const removeItem = newArrivalList.splice(itemIndex, 1);
+
+                }
+            } else if (event === 'new-arrival') {
+                for (let x = 0; x < 8; x++) {
+                    let bestSellerList = data.product;
+                    let soldItem = 0;
+                    let count = 0;
+                    let itemIndex = 0;
+
+                    for (let item of bestSellerList) {
+                        if (soldItem < item.rating.sold) {
+                            soldItem = item.rating.sold;
+                            itemIndex = count;
+                        }
+                        count++;
+                    }
+                    const newArrival = document.getElementById('new-arrival');
+                    const productContainer = document.createElement('div');
+                    productContainer.className = 'col-md-6 col-lg-3 mb-4';
+                    productContainer.innerHTML = `<div class="card">
+                                                            <div class="inner"><a href="#"><img class="card-img-top" src="${bestSellerList[itemIndex].image[0]}"></a></div>
+                                                            <div class="card-body p-2">
+                                                                    <div>
+                                                                            <p class="card-text fw-bolder fs-4 text-center">${bestSellerList[itemIndex].title}</p><span class="fs-4">${bestSellerList[itemIndex].price.currency} ${bestSellerList[itemIndex].price.value}</span><br>
+                                                                            <span class="text-dark fs-6">${bestSellerList[itemIndex].stock != 0 ? 'Available' : 'Not Available'}</span>
+                                
+                                                                            <div class="d-flex justify-content-start text-warning my-4">
+                                    
+                                                                                    <div><i class="fa-solid fa-star"></i></div>
+                                                                                    <div><i class="fa-solid fa-star"></i></div>
+                                                                                    <div><i class="fa-solid fa-star"></i></div>
+                                                                                    <div><i class="fa-solid fa-star"></i></div>
+                                                                                    <div><i class="fa-solid fa-star-half-stroke"></i></div>
+                                                                                    <span class="text-dark fs-6 ms-2">| ${bestSellerList[itemIndex].rating.sold} Sold</span>
+                                                                            </div>
+                                                                    </div>
+                                                            </div>
+                                                    </div>`;
+                    newArrival.appendChild(productContainer);
+
+                    const removeItem = bestSellerList.splice(itemIndex, 1);
+
+                }
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+
+
+
+
+
+
 
                         // CATEGORY PRODUCT SCROLLING
 
